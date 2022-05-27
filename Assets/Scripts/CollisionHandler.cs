dbac6f24bc8f3330def4fbb479185971bb0ee7ef
Emopusta <emreduman225@hotmail.com>
2022-05-27 13:12:01 +0300

    harun

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip crashAudio;
+    [SerializeField] AudioClip successAudio;
+    
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+    
+
+    AudioSource audioS;
+
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    void Start()
+    {
+
+        audioS = GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+    void RespondToDebugKeys()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+        else if (Input.GetKey(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toggle collision
+            Debug.Log("Collision Cheatcode: " + collisionDisabled);
+;        }
+
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning) { return; }
+        else
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    break;
+                case "Finish":
+                    StartLandingSequence();
+                    break;
+                case "Fuel":
+                    Debug.Log("added fuel.");
+                    break;
+                default:
+                    if (!collisionDisabled)
+                    {
+                        StartCrashSequence();
+                    }
+                    break;
+
+
+            }
+        }
+    }
+    void StartCrashAudio() // todo fix the audio for proper timing
+    {
+        audioS.Stop();
+        audioS.PlayOneShot(crashAudio);
+    }
+    void StartSuccessAudio()
+    {
+        audioS.Stop();
+        audioS.PlayOneShot(successAudio);
+    }
+    void StartCrashSequence()
+    {
+
+        crashParticles.Play(); // play the crash particles
+        isTransitioning = true;
+        StartCrashAudio();
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+        
+    }
+
+    void StartLandingSequence()
+    {
+        successParticles.Play(); // play the success particles
+        isTransitioning = true;
+        StartSuccessAudio(); // starts success audio
+        GetComponent<Movement>().enabled = false; // make player can not move
+        Invoke("LoadNextLevel", levelLoadDelay); // loads next level in particular delay
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        int totalNumberOfLevels = SceneManager.sceneCountInBuildSettings;
+        if (nextSceneIndex == totalNumberOfLevels)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex); 
+    }
+   
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] float rotateThrust = 80f;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem leftSideEngineParticles;
+    [SerializeField] ParticleSystem rightSideEngineParticles;
+
+
+    Rigidbody rb;
+    AudioSource audioS;
+
+
+    float degree;
+
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+        audioS = GetComponent<AudioSource>();
+        audioS.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ThrusterAudio()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && audioS.isPlaying != true)
+        {
+            audioS.PlayOneShot(mainEngine);
+        }
+        else if (Input.GetKeyUp(KeyCode.W) && audioS.isPlaying == true)
+        {
+            audioS.Stop();
+        }
+    }
+
+    
+    void ProcessThrust()
+    {
+        //ThrusterAudio();
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            StartThrusting();
+        }
+        else
+        {
+            StopThrusting();
+        }
+
+    }
+    void ProcessRotation()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            StartRotatingRight();
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            StartRotatingLeft();
+        }
+        else
+        {
+            StopRotating();
+        }
+
+    }
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioS.isPlaying)
+        {
+            //audioS.Play();
+            audioS.PlayOneShot(mainEngine);
+
+        }
+        if (!mainEngineParticles.isPlaying)
+        {
+            mainEngineParticles.Play();
+        }
+    }
+    void StopThrusting()
+    {
+        audioS.Stop();
+        mainEngineParticles.Stop();
+    }
+    
+
+    void StartRotatingRight()
+    {
+        ApplyRotation(-rotateThrust);
+        if (!rightSideEngineParticles.isPlaying)
+        {
+            rightSideEngineParticles.Play();
+        }
+    }
+
+    void StartRotatingLeft()
+    {
+        //rb.MoveRotation(Quaternion.Euler(0, 0, degree += rotateThrust));
+        ApplyRotation(rotateThrust);
+        if (!leftSideEngineParticles.isPlaying)
+        {
+            leftSideEngineParticles.Play();
+        }
+    }
+
+    void StopRotating()
+    {
+        rightSideEngineParticles.Stop();
+        leftSideEngineParticles.Stop();
+    }
+
+    void ApplyRotation(float rotationWay)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationWay * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so the physics system can't take over
+       
+    }
+}
